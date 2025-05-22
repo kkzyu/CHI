091c147e7f643f4839393b4a40b287b7fdd31a38
ALANNIIDO <15464813+alanniido@user.noreply.gitee.com>
2025-05-22 11:16:21 +0800

    some commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/chi-dashboard/',
+  base:'/CHI/',
   plugins: [
     vue(),
     vueDevTools(),
